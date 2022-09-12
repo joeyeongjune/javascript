fd754b381fd8d5c97a44c94e1b95865ee5ff4eac
yojucho <yojucho.jp@gmail.com>
2022-09-13 00:11:27 +0900

    Practice JavaScript

--- MOMENTUM/app.js
+++
@@ -1,5 +0,0 @@
-const hello = document.querySelector("div.hello:first-child h1");
-
-console.dir(hello);
-
-hello.style.backgroundColor = "orange";
---
+++ MOMENTUM/calculator.js
@@ -0,0 +1,21 @@
+const calculator = {
+  addition: function (a, b) {
+    console.log(a + b);
+  },
+  subtraction: function (a, b) {
+    console.log(a - b);
+  },
+
+  multiplication: function (a, b) {
+    console.log(a * b);
+  },
+
+  division: function (a, b) {
+    console.log(a / b);
+  },
+};
+
+calculator.addition(5, 5);
+calculator.subtraction(10, 5);
+calculator.multiplication(5, 5);
+calculator.division(10, 5);
